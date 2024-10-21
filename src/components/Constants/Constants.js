e6980ff1d7ecd9d5ff7f4a9e8a2b92a28ab0df33
boomieindahouse <prawitsanat@hotmail.com>
2024-10-21 11:11:24 +0700

    edit constants

--- src/components/Constants/Constants.js
+++ src/components/Constants/Constants.js
@@ -2,67 +2,67 @@ export const skillsData = [
   {
     name: "HTML",
     level: 50,
-    icon: "../src/assets/icons/html.png",
+    icon: "/icons/html.png",
   },
   {
     name: "CSS",
     level: 60,
-    icon: "../src/assets/icons/css.png",
+    icon: "/icons/css.png",
   },
   {
     name: "JavaScript",
     level: 40,
-    icon: "../src/assets/icons/javascript.png",
+    icon: "/icons/javascript.png",
   },
   {
     name: "React",
     level: 30,
-    icon: "../src/assets/icons/react.png",
+    icon: "/icons/react.png",
   },
   {
     name: "PHP",
     level: 30,
-    icon: "../src/assets/icons/php.png",
+    icon: "/icons/php.png",
   },
   {
     name: "SQL",
     level: 30,
-    icon: "../src/assets/icons/sql.png",
+    icon: "/icons/sql.png",
   },
   {
     name: "Flutter",
     level: 30,
-    icon: "../src/assets/icons/flutter.png",
+    icon: "/icons/flutter.png",
   },
   {
     name: "Bootstrap",
     level: 40,
-    icon: "../src/assets/icons/bootstrap.png",
+    icon: "/icons/bootstrap.png",
   }
 ];
 
 export const toolsData = [
   {
     name: "Windows",
-    icon: "../src/assets/icons/windows.png",
+    icon: "/icons/windows.png",
   },
   {
     name: "vsCode",
-    icon: "../src/assets/icons/vscode.png",
+    icon: "/icons/vscode.png",
   },
   {
     name: "Git",
-    icon: "../src/assets/icons/git.png",
+    icon: "/icons/git.png",
   },
   {
     name: "Figma",
     level: "Beginner",
-    icon: "../src/assets/icons/figma.png",
+    icon: "/icons/figma.png",
   },
   {
     name: "Eclipse",
     level: "Beginner",
-    icon: "../src/assets/icons/eclipse.png",
+    icon: "/icons/eclipse.png",
   }
 ];
 
@@ -70,19 +70,19 @@ export const languageSkills = [
   { 
     name: 'Read', 
     level: 80, 
-    icon: "../src/assets/icons/reading.png", 
+    icon: "/icons/reading.png", 
     label: 'Advanced' 
   },
   { 
     name: 'Write', 
     level: 60,
-    icon: "../src/assets/icons/write.png",
+    icon: "/icons/write.png",
     label: 'Intermediate' 
   },
   { 
     name: 'Listen', 
     level: 70, 
-    icon: "/listen.png", 
+    icon: "/icons/listen.png", 
     label: 'Intermediate' 
   },
   { 
