970e9c28f158a815a70346cd3c072940ca2f6589
boomieindahouse <prawitsanat@hotmail.com>
2024-11-17 17:25:18 +0700

    edit constants

--- src/components/Constants/Constants.js
+++ src/components/Constants/Constants.js
@@ -126,7 +126,7 @@ export const portfolios = [
       "7.png",
       "8.png",
     ],
-    dev: ["Altorouter php", "Javascript", "CSS", "MySql"],
+    dev: ["Altorouter PHP", "Javascript", "CSS", "MySql"],
     functions: [
       "Authentication: Users can sign up, log in, and log out.",
       "Profile Management: Users can update their profile information and profile picture.",
@@ -170,7 +170,7 @@ export const portfolios = [
       "5.png",
       "6.png",
     ],
-    dev: ["Altorouter php", "Javascript", "CSS", "MySql"],
+    dev: ["Altorouter PHP", "Javascript", "CSS", "MySql"],
     functions: [
       "Able to book resort rooms.",
       "Capable of managing resort rooms.",
